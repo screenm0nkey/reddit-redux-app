61fc4bdd4ba1b84b3c2731d957a792e31fb00830
Nick Lowman <digital@nicklowman.co.uk>
2016-09-17 08:31:41 +0100

    Updated build for heroku

--- prod-server.js
+++ prod-server.js
@@ -3,9 +3,6 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(__dirname + 'dist'));
-app.use(express.static(__dirname + 'dist/static'));
-
 app.get(/bundle.js$/, function (req, res) {
   res.sendFile(path.resolve(__dirname, 'dist/static/bundle.js'));
 });
